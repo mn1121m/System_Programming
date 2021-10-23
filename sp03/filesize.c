2a2c525f033189324dcf7ffcc6743d0758e49d13
mn1121m <83820185+mn1121m@users.noreply.github.com>
2021-10-23 20:53:08 +0900

    updated sp03, sp04

---
+++ sp03/filesize.c
@@ -0,0 +1,17 @@
+/*
+file: filesize.c
+description:
+		prints size of passwd file
+*/
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main()
+{
+	struct stat infobuf;	/* place to store info	*/
+
+	if(stat( "/etc/passwd", &infobuf) == -1) /* get info */
+	    	perror("/etc/passwd");
+	else
+	    	printf(" The size of /etc/passwd is %d\n", infobuf.st_size);
+}
---
+++ sp03/ls1.c
@@ -0,0 +1,42 @@
+/*
+file: ls1.c
+author: 2018117610_moon jun yong
+datatime: 2021-10-22 (updated)
+description:
+		purpose - list contents of directory or directories
+		action - if no arhs, use . else list files in args
+*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+void do_ls( char []);
+
+int main(int ac, char *av[])
+{
+	if( ac == 1)
+		do_ls(".");
+	else
+	    while(--ac) {
+		printf("%s: \n", *++av);
+		do_ls(*av);
+	    }
+}
+
+void do_ls( char dirname[] )
+{
+/*	list files in directory called dirname	*/
+	DIR		*dir_ptr;	/* the directory	*/
+	struct dirent	*direntp;	/* each entry		*/
+
+	if( (dir_ptr = opendir(dirname) )== NULL)
+	    	fprintf(stderr, "ls1: cannot open %s\n", dirname);
+	else{
+		while( ( direntp = readdir(dir_ptr) ) != NULL)
+		    	printf("%s\n", direntp->d_name);
+		closedir(dir_ptr);
+	}
+}
+
+
+
